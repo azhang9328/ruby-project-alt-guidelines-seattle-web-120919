b4f311c5f6efc0a05fd1c7374cc17dae5d1235a0
Azhang9328 <azhang9328@gmail.com>
2020-01-01 18:47:30 -0800

    parse file

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,4 +1,5 @@
 class CommandLineInterface
+
     def run
         main_menu
     end
@@ -6,7 +7,19 @@ class CommandLineInterface
     def main_menu
         puts "Test Name"
         puts ""
-        proxy_file_path = gets.chomp
+        puts "Please give path to proxy file."
+        proxies = parse_proxy_file(get_proxy_file)
+        puts "Add to proxies?"
+        puts "y / n"
+        proxies.each do |proxy|
+            
+        end
+    end
+
+    def get_proxy_file
+        test_path = "/Users/azhang/Downloads/socks5_proxies.txt"
+        # proxy_file_path = gets.chomp
+        proxy_file_path = test_path
         puts ""
         until proxy_file_path.include?("Users")
             puts "Enter valid file path"
@@ -14,6 +27,7 @@ class CommandLineInterface
             proxy_file_path = gets.chomp
         end
         proxy_file = File.readlines(proxy_file_path)
-        p proxy_file
+        proxy_file
     end
+
 end
